bf55645a965a01c8386475da6b953e8e4de71e59
imran <mdemranali3656vu@gmail.com>
2025-12-24 19:54:44 +0600

    first commit

---
+++ programm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("number");
+    scanf("%d", &a);
+    printf("number");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf(" %d ", sum);
+    return 0;
+}
