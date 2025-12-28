27b5a4dad2d49dcf011d8be4ea8d98b0171756ef
imran <mdemranali3656vu@gmail.com>
2025-12-29 01:31:52 +0600

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+const totalrice=480;
+const month=30;
+const perday=4;
+const totalday=totalrice/perday;
+const totalmonth=totalrice/month;
+console.log(totalday);
+console.log(totalmonth);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const totaltaka=60;
+const ueseeverytime=15;
+const totaluese=totaltaka/ueseeverytime;
+
+console.log(totaluese);
